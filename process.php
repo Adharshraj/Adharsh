804da181301bd166d6631a6844a7c2c8720cac33
Adharshraj <119029013+Adharshraj@users.noreply.github.com>
2022-12-27 21:42:25 +0530

    Update process.php

--- process.php
+++ process.php
@@ -3,15 +3,7 @@
 
 
   if(isset($_POST['submit'])){
-  $to ="adharshraj35@gmail.com";
-  $subject = $_POST['subject'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  $headers = "From: " .$email. "\r\n".
-  
-  
-  mail($to,$subject,$message,$headers);
-  header("Location: contact.html");
+   echo"Hello Guys";
   
   
   }
